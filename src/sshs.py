96cc37800958da0e0ce32e9c8c9dc8104b89a1c6
tssan <piotr.karasinski@gmail.com>
2020-01-21 19:10:57 +0100

    show_list, host del/con

--- src/sshs.py
+++ src/sshs.py
@@ -9,7 +9,8 @@ HOSTS_FILE = 'hosts.json'
 
 EMPTY_HOSTS_FILE = {
     'aliases': [],
-    'hosts': {}
+    'hosts': {},
+    'users': {}
 }
 
 
@@ -21,32 +22,49 @@ class SshSelectUI:
         with open(hosts_path) as hosts_file:
             self.hosts = json.load(hosts_file)
 
+    def update(self):
+        with open(self.hosts_path, 'w') as hosts_file:
+            json.dump(self.hosts, hosts_file)
+
     def menu(self):
         LAST = 'use last connection'
         LIST = 'list all hosts'
         NEW = 'add new host'
+        EXIT = 'exit'
 
         cli = b.Bullet(
-            prompt='What you want to do?',
+            prompt='*',
             choices=[
                 LAST,
                 LIST,
-                NEW
+                NEW,
+                EXIT
             ]
         )
         result = cli.launch()
 
         if result == NEW:
             self.add_host()
+        elif result == LIST:
+            self.show_list()
+        elif result == EXIT:
+            return
 
     def add_host(self):
+        user = os.getlogin()
+
         cli = b.VerticalPrompt([
             b.Input('Alias: '),
-            b.Input('Hostname/IP: ')
+            b.Input('Hostname/IP: '),
+            b.Input(f'Username (default:{user}): ', pattern=r'.*')
         ])
         result = cli.launch()
         alias = result[0][1]
         host = result[1][1]
+        username = result[2][1]
+
+        if username == '':
+            username = user
 
         if alias in self.hosts['aliases']:
             print('Host already exists')
@@ -54,9 +72,42 @@ class SshSelectUI:
 
         self.hosts['aliases'].append(alias)
         self.hosts['hosts'][alias] = host
+        self.hosts['users'][alias] = username
 
-        with open(hosts_path, 'w') as hosts_file:
-            json.dump(self.hosts, hosts_file)
+        self.update()
+
+    def show_list(self):
+        if len(self.hosts['aliases']) == 0:
+            print('Hosts list is empty.')
+            return self.menu()
+
+        cli = b.Bullet(
+            prompt='Available hosts:',
+            choices=self.hosts['aliases']
+        )
+        alias = cli.launch()
+
+        CON_HOST = 'connect'
+        DEL_HOST = 'delete host'
+        BACK = 'back'
+        host_cli = b.Bullet(
+            prompt=f'Host {alias} options:',
+            choices=[CON_HOST, DEL_HOST, BACK]
+        )
+        result = host_cli.launch()
+
+        if result == CON_HOST:
+            user = self.hosts['users'][alias]
+            host = self.hosts['hosts'][alias]
+            os.system(f'ssh {user}@{host}')
+        elif result == DEL_HOST:
+            self.hosts['aliases'].remove(alias)
+            del self.hosts['hosts'][alias]
+            del self.hosts['users'][alias]
+            self.update()
+            return self.show_list()
+        elif result == BACK:
+            return self.menu()
 
 
 if __name__ == '__main__':
